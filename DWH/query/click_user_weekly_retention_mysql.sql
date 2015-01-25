7678ba1425546ce4d1db112801bdd9c499e34e5d
oranguice <breeze0728@gmail.com>
2015-01-25 22:57:50 +0800

    点击用户周回访

---
+++ DWH/query/click_user_weekly_retention_mysql.sql
@@ -0,0 +1,19 @@
+select col_flag, count(*) as col_num
+	from
+	(
+		select col_uid, sum(col_flag) as col_flag 
+		from 
+		( 
+				select col_uid, 1 as col_flag 
+					from subscribe.tb_daily_users
+					where p_date >= {date-13} and p_date <= {date-7} and col_udid is not null and col_feed_clicks > 0
+				group by col_uid
+			union all 
+				select col_uid, 2 as col_flag 
+					from subscribe.tb_daily_users
+					where p_date >= {date-6} and p_date <= {date} and col_udid is not null and col_feed_clicks > 0
+				group by col_uid
+		)ta 
+		group by col_uid
+	)tb
+group by col_flag
