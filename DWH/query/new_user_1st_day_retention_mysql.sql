484d030745ac75d515b4f94e9a4d832758de2ea5
oranguice <breeze0728@gmail.com>
2015-01-25 22:56:10 +0800

    新用户次日回访

---
+++ DWH/query/new_user_1st_day_retention_mysql.sql
@@ -0,0 +1,19 @@
+select replace(date_add(p_date, interval 1 day),'-','') as p_date, col_flag, count(*) as col_num
+	from
+	(
+		select p_date, col_uid, sum(col_flag) as col_flag 
+		from 
+		( 
+				select concat(substring(p_date, 1, 4), '-', substring(p_date, 5, 2), '-', substring(p_date, 7, 2)) as p_date, col_uid, 1 as col_flag 
+					from subscribe.tb_daily_users
+					where col_new_user = 1 and p_date = {date-1}
+				group by concat(substring(p_date, 1, 4), '-', substring(p_date, 5, 2), '-', substring(p_date, 7, 2)), col_uid
+			union all 
+				select date_sub(concat(substring(p_date, 1, 4), '-', substring(p_date, 5, 2), '-', substring(p_date, 7, 2)), interval 1 day) as p_date, col_uid, 2 as col_flag 
+					from subscribe.tb_daily_users
+					where p_date = {date}
+				group by date_sub(concat(substring(p_date, 1, 4), '-', substring(p_date, 5, 2), '-', substring(p_date, 7, 2)), interval 1 day), col_uid
+		)ta 
+		group by p_date, col_uid
+	)tb
+group by replace(date_add(p_date, interval 1 day),'-',''), col_flag
