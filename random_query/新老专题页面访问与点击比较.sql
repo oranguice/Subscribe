40f99d4a8ffb9cd6299a9f0d3ad1230071086267
oranguice <breeze0728@gmail.com>
2015-02-07 19:55:52 +0800

    修改日期为 {date}

--- random_query/新老专题页面访问与点击比较.sql
+++ random_query/新老专题页面访问与点击比较.sql
@@ -6,14 +6,14 @@ count(case when a.col_url like '/subset_profil%' then a.col_uid else null end)
 from (
     select p_date, col_uid, col_url
     from android.tb_page_show
-    where p_date >= 20150201 and p_date <= 20150204 and ((col_url like '/topic?id=%' and length(col_url) > 15) or col_url like '/subset_profil%')) a
+    where p_date = {date} and ((col_url like '/topic?id=%' and length(col_url) > 15) or col_url like '/subset_profil%')) a
 join (
     select p_date, col_uid
     from subscribe.tb_daily_users
-    where p_date >= 20150201 and p_date <= 20150204 and col_new_user = 0) b
+    where p_date = {date} and col_new_user = 0) b
 on a.p_date = b.p_date and a.col_uid = b.col_uid
 group by a.p_date
---订阅用户中看到：新专题人数；次数；老专题人数；次数
+--New-订阅用户中看到：新专题人数；次数；老专题人数；次数
 
 
 
@@ -25,15 +25,15 @@ count(case when a.col_url like '/subset_profil%' then a.col_udid else null end)
 from (
     select p_date, col_udid, col_url
     from android.tb_page_show
-    where p_date >= 20150201 and p_date <= 20150204 and ((col_url like '/topic?id=%' and length(col_url) > 15) or col_url like '/subset_profil%')) a
+    where p_date = {date} and ((col_url like '/topic?id=%' and length(col_url) > 15) or col_url like '/subset_profil%')) a
 left outer join (
     select p_date, col_udid
     from subscribe.tb_daily_users
-    where p_date >= 20150201 and p_date <= 20150204) b
+    where p_date = {date}) b
 on a.p_date = b.p_date and a.col_udid = b.col_udid
 where b.col_udid is null
 group by a.p_date
---非订阅用户中看到：新专题人数；次数；老专题人数；次数
+--New-非订阅用户中看到：新专题人数；次数；老专题人数；次数
 
 
 
@@ -49,14 +49,14 @@ sum(case when a.col_url like '/subset_profil%' and (a.col_name = '安装' or a.c
 from (
     select p_date, col_uid, col_url, col_name
     from android.tb_click
-    where p_date >= 20150201 and p_date <= 20150204 and ((col_url like '/topic?id=%' and length(col_url) > 15) or col_url like '/subset_profil%')) a
+    where p_date = {date} and ((col_url like '/topic?id=%' and length(col_url) > 15) or col_url like '/subset_profil%')) a
 join (
     select p_date, col_uid
     from subscribe.tb_daily_users
-    where p_date >= 20150201 and p_date <= 20150204 and col_new_user = 0) b
+    where p_date = {date} and col_new_user = 0) b
 on a.p_date = b.p_date and a.col_uid = b.col_uid
 group by a.p_date
---订阅用户中点击：新专题关注、内容、消费、下一个、更多；老专题关注、内容、消费
+--New-订阅用户中点击：新专题关注、内容、消费、下一个、更多；老专题关注、内容、消费
 
 
 select a.p_date,
@@ -71,13 +71,13 @@ sum(case when a.col_url like '/subset_profil%' and (a.col_name = '安装' or a.c
 from (
     select p_date, col_udid, col_url, col_name
     from android.tb_click
-    where p_date >= 20150201 and p_date <= 20150204 and ((col_url like '/topic?id=%' and length(col_url) > 15) or col_url like '/subset_profil%')) a
+    where p_date = {date} and ((col_url like '/topic?id=%' and length(col_url) > 15) or col_url like '/subset_profil%')) a
 left outer join (
     select p_date, col_udid
     from subscribe.tb_daily_users
-    where p_date >= 20150201 and p_date <= 20150204) b
+    where p_date = {date}) b
 on a.p_date = b.p_date and a.col_udid = b.col_udid
 where b.col_udid is null
 group by a.p_date
---非订阅用户中点击：新专题关注、内容、消费、下一个、更多；老专题关注、内容、消费
+--New-非订阅用户中点击：新专题关注、内容、消费、下一个、更多；老专题关注、内容、消费
 
