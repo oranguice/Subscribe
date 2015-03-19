63cdbb95a111b8878b12b34d809b0bf04f639601
oranguice <breeze0728@gmail.com>
2015-03-19 12:03:38 +0800

    小修改 & 4.25 发版后小流量数据

--- DWH/query/daily_launches.sql
+++ DWH/query/daily_launches.sql
@@ -1,4 +1,4 @@
-select p_date, sum(col_launches), count(DISTINCT col_uid)
+select p_date, count(distinct col_uid), sum(col_launches)
 from tb_daily_users
 where p_date >= {date-6} and p_date <= {date} and col_launches > 0 and col_udid is not null
 group by p_date
